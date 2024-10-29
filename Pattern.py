f5628412cf77236fb3728c0ff7fc426472a2ec08
Jp21212 <Jamesp2814@tuckahoeschools.org>
2024-10-29 08:11:48 -0400

    Pattern.py

--- Pattern.py
+++ Pattern.py
@@ -1,1 +1,17 @@
+import turtle
+t = turtle.Turtle()
+t.shape(name="turtle")
 
+
+t.speed(500) # 1:slowest, 3:slow, 5:normal, 10:fast, 0:fastest
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+
+for i in range(5000):
+  t.forward(i)
+  t.left(500)
